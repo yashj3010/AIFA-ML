12b1f1508ae7b00eebde160d80579dee9bbeefec
prachi-ec <71262691+prachi-ec@users.noreply.github.com>
2020-09-25 15:11:14 +0530

    Add files via upload

---
+++ DataReduction.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import TimeStampConverter as tsc
+import os
+
+file_path=r"C:\Users\prach\Downloads\Logger 1"   #path of csvs
+output_path=r"C:\Users\prach\Downloads"
+
+def meanobservation(inpath, outpath):
+
+    df = tsc.TimeStampFormatter(inpath)
+    res_df = df.groupby(pd.Grouper(key="TimeStamp")).mean()
+    res_df.to_csv(os.path.join(outpath,"res_df.csv"))
+
+
+if __name__ == "__main__":
+    meanobservation(file_path, output_path)
---
+++ TimeStampConverter.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+
+file_path=r"C:\Users\prach\Downloads\Logger 1"   #path of csvs
+
+
+### Fuction to convert timestamp from object datatype to datetime
+
+
+def TimeStampFormatter(path):
+    raw_csvs = [f for f in os.listdir(path)]
+    for i in raw_csvs:
+        df= pd.read_csv(os.path.join(path,i))
+        df['TimeStamp'] = pd.to_datetime(df['TimeStamp'], format="%H:%M")   #Edit format according to the need currently output in HH:MM form
+        return df
+
+if __name__ == "__main__":
+    TimeStampFormatter(file_path)
+
+
